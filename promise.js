dc78d11e5b286e88229d2783a38f99e79f25f777
vreymond <valentinreymond@gmail.com>
2019-03-31 11:18:49 +0200

    Adding ES8 version using async/await

--- promise.js
+++ promise.js
@@ -1,7 +1,8 @@
 const wordnikAPI = "https://api.wordnik.com/v4/words.json/randomWord?&minLength=5&maxLength=-1&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7";
 const giphyAPI = "https://api.giphy.com/v1/gifs/search?rating=PG&api_key=dc6zaTOxFJmzC&q=";
 
-
+//ES6 version
+/*
 function setup() {
   noCanvas();
 
@@ -15,5 +16,30 @@ function setup() {
     .then(json => {
         createImg(json.data[0].images['fixed_height_small'].url)
     })
-    .catch(err => console.log(err))
+    .catch(err => console.error(err))
+}
+*/
+
+//ES8 version
+function setup() {
+    noCanvas();
+    wordGIF()
+    .then(results => {
+        createP(results.word);
+        createImg(results.img);
+    })
+    .catch(err => console.error(err));
+}
+
+async function wordGIF() {
+    let response1 = await fetch(wordnikAPI);
+    let json1 = await response1.json();
+    let response2 = await fetch(giphyAPI + json1.word);
+    let json2 = await response2.json();
+    let img_url = json2.data[0].images['fixed_height_small'].url
+
+    return {
+        word: json1.word,
+        img: img_url
+    }
 }
